3f56f96313cbd639760a1a97fef77491cc2b19aa
Michael <mfye@stetson.edu>
2013-12-03 17:25:57 -0500

    Added the cashier, customer generator and uniform customer generator that I've changed.
    Edit:
    	Seriously Git? You bloody git...
    		
    Edit B:
    	Seriously Chris? XD

--- Queue/src/queuemodel/CustomerGenerator.java
+++ Queue/src/queuemodel/CustomerGenerator.java
@@ -1,42 +1,107 @@
 package queuemodel;
 
-public class CustomerGenerator extends UniformCustomerGenerator implements Runnable 
+import java.util.Random;
+
+public class CustomerGenerator extends UniformCustomerGenerator implements Runnable
 {
-	private int myMaxTimeBetweenCustomers;
-	
+	private int myMaxTimeBetweenCustomers, myRandomTimeBetweenCustomers, myMaxNumberCustomers;
+	private Random myRandom;
+	private ServiceQueue myShortestServiceQueue;
+	private Thread myThread;
+
+	/**
+	 * Constructor for the CustomerGenerator. Calls the super constructor and
+	 * passes the maxTimeBetweenCustomers and serviceQueueManager. Defaults the
+	 * myMaxNumberCustomers value to 500. Sets the max time between customers to
+	 * maxTimeBetweenCustomers.
+	 * 
+	 * @param maxTimeBetweenCustomers
+	 * @param serviceQueueManager
+	 */
 	public CustomerGenerator(int maxTimeBetweenCustomers, ServiceQueueManager serviceQueueManager)
 	{
-	    super(maxTimeBetweenCustomers, serviceQueueManager);
+		super(maxTimeBetweenCustomers, serviceQueueManager);
 		myMaxTimeBetweenCustomers = maxTimeBetweenCustomers;
+		myMaxNumberCustomers = 500;
+		myThread = new Thread(this);
 	}
-	
+
+	/**
+	 * Returns the time the generator takes between generation of customers,
+	 * using the super class generateTimeBetweenCustomers()
+	 * 
+	 * @return Int generated time
+	 */
 	public int generateTimeBetweenCustomers()
 	{
-		return 0;
+		return super.generateTimeBetweenCustomers();
 	}
-	
+
+	/**
+	 * generateCustomer method. Creates a new Customer and returns it.
+	 * 
+	 * @return Customer newCustomer
+	 */
 	public Customer generateCustomer()
 	{
-		return null;
+		Customer newCustomer = new Customer();
+		return newCustomer;
 	}
-	
+
 	public void run()
 	{
-		
+		while (myMaxNumberCustomers > 0)
+		{
+			myShortestServiceQueue = super.getSQM().determineShortestQueue();
+			myShortestServiceQueue.insertCustomer(this.generateCustomer());
+			myMaxNumberCustomers--;
+		}
+
+		try
+		{
+			Thread.sleep(this.getTimeBetweenCustomers());
+		} catch (InterruptedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+
 	public void start()
 	{
-		
+		try
+		{
+			myThread.start();
+		} catch (IllegalThreadStateException e)
+		{
+			System.out.println("Thread already started");
+		}
 	}
-	
-	public Customer getCustomer()
+
+	/**
+	 * Returns the max time between customers
+	 * 
+	 * @return int myMaxTimeBetweenCustomers
+	 */
+	public int getMyMaxTimeBetweenCustomers()
 	{
-		return null;
+		return myMaxTimeBetweenCustomers;
 	}
-	
+
+	/**
+	 * Generates a pseudorandom time that the generator will take between
+	 * customer generation, bounded by the maxTimeBetweenCustomers Calls
+	 * generateTimeBetweenCustomers()
+	 * 
+	 * @return int random time between customers.
+	 */
 	public int getTimeBetweenCustomers()
 	{
-		return 0;
+		return this.generateTimeBetweenCustomers();
+	}
+	
+	public void setMaxNumberCustomers(int maxNumberCustomers)
+	{
+		myMaxNumberCustomers = maxNumberCustomers;
 	}
 }
--- Queue/src/queuemodel/UniformCustomerGenerator.java
+++ Queue/src/queuemodel/UniformCustomerGenerator.java
@@ -5,14 +5,21 @@ import java.util.Random;
 public class UniformCustomerGenerator
 {
 	private Random myRandom;
+	private ServiceQueueManager mySQM;
 
 	public UniformCustomerGenerator(int maxTimeBetweenCustomers, ServiceQueueManager serviceQueueManager)
 	{
 		myRandom = new Random(maxTimeBetweenCustomers);
+		mySQM = serviceQueueManager;
 	}
 	
 	public int generateTimeBetweenCustomers()
 	{
 		return myRandom.nextInt();
+	}
+	
+	public ServiceQueueManager getSQM()
+	{
+		return mySQM;
 	}
 }
